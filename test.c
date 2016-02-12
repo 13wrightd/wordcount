04d8ae0d104fa83adcdbd840f19c0d189a4e6d91
elg5195 <evan.gutman@gmail.com>
2016-02-11 20:36:42 -0500

    beginning linked list implementation

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 struct word_t {
 	char *word;
@@ -12,10 +13,17 @@ int main(int argc, char *argv[]) {
 	int i = 0;
 	FILE *fp;
 	fp = fopen(argv[1], "r");
-
 	char *str;
 	char c;
 	int numChar = 0;
+	struct word_t *head;
+	struct word_t *tail;
+	struct word_t *curr;
+	bool found = false;
+
+	head = (struct word_t *)malloc(sizeof(struct word_t));
+	tail = head;
+	curr = head;
 
 	str = (char *)malloc(sizeof(char));
 	bool badCharacter=0;
@@ -32,10 +40,29 @@ int main(int argc, char *argv[]) {
 			else if((c == 32 || c == 10) && !badCharacter){
 				badCharacter=true;
 				str[numChar]='\0';
-				printf("%s ", str);
 				numChar = 0;
+
+				while(curr.next != NULL){
+					if(strcmp(str, curr.word) == 0){
+						curr.count++;
+					}
+					else if(strcmp(str, curr.word) > 0){
+						if(curr.next == NULL){
+							curr.next = (struct word_t *)malloc(sizeof(struct word_t));
+							strcpy(curr.next.word, str);
+							curr.next.count++;
+
+						}
+						curr = curr.next;
+
+					}
+
+
+				}
+
 				free(str);
 				str = (char *)malloc(sizeof(char));
+					}
 			}
 			
 		}
