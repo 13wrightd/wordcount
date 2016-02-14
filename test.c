101ed4ed3ff18d0d7d6ae09643df7254477b2aa1
elg5195 <evan.gutman@gmail.com>
2016-02-14 17:07:57 -0500

    random

--- test.c
+++ test.c
@@ -10,69 +10,82 @@ struct word_t {
 };
 
 int main(int argc, char *argv[]) {
-	int i = 0;
-	FILE *fp;
-	fp = fopen(argv[1], "r");
-	char *str;
-	char c;
-	int numChar = 0;
-	struct word_t *head;
-	struct word_t *tail;
-	struct word_t *curr;
-	bool found = false;
+    int i = 0;
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    char *str;
+    char c;
+    int numChar = 0;
+    struct word_t *head;
+    //struct word_t *tail;
+    struct word_t *curr;
+    bool found = false;
 
-	head = (struct word_t *)malloc(sizeof(struct word_t));
-	tail = head;
-	curr = head;
+    head = (struct word_t *)malloc(sizeof(struct word_t));
+    //tail = head;
+    curr = head;
 
-	str = (char *)malloc(sizeof(char));
-	bool badCharacter=0;
+    str = (char *)malloc(sizeof(char));
+    bool badCharacter=0;
 
-	if (fp) {
-		while ((c=fgetc(fp))!=EOF){
-			if((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57)){
-				str[numChar] = tolower(c);
-				numChar++;
-				str = (char *)realloc(str, numChar*sizeof(char));
-				badCharacter=false;
-			}
+    if (fp) {
+        while ((c=fgetc(fp))!=EOF){
+            if((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57)){
+                str[numChar] = tolower(c);
+                numChar++;
+                str = (char *)realloc(str, numChar*sizeof(char));
+                badCharacter=false;
+            }
+
+            else if((c == 32 || c == 10) && !badCharacter){
+                badCharacter=true;
+                str[numChar]='\0';
+                numChar = 0;
+
+                while(!found){
+                    if(strcmp(str, curr->word) == 0){
+                            curr->count++;
+                            found = true;
+                    }
+                    else if(strcmp(str, curr->word) > 0){
+                            if(curr->next == NULL){
+                                    curr->next = (struct word_t *)malloc(sizeof(struct word_t));
+                                    strcpy(curr->next->word, str);
+                                    curr->next->count++;
+                                    found = true;
+                            }
+                            else{
+                                    curr = curr->next;
+                            }
+
+                    }
+                    else{
+                            struct word_t *newNode;
+                            newNode = (struct word_t *)malloc(sizeof(struct word_t));
+                            strcpy(newNode->word, str);
+                            newNode->count++;
+                            newNode->next = curr->next;
+                            curr->next = newNode;
+                            found = true;
+                    }
+
+                }
+                    curr = head;
+                    found = false;
+
+            }
+
+            free(str);
+            str = (char *)malloc(sizeof(char));
+        }
+    }
+    while(curr->next != NULL){
+            printf("%s, %d", curr->word, curr->count);
+            curr = curr->next;
+    }
+    return 0;
 			
-			else if((c == 32 || c == 10) && !badCharacter){
-				badCharacter=true;
-				str[numChar]='\0';
-				numChar = 0;
+}
 
-				while(curr.next != NULL){
-					if(strcmp(str, curr.word) == 0){
-						curr.count++;
-					}
-					else if(strcmp(str, curr.word) > 0){
-						if(curr.next == NULL){
-							curr.next = (struct word_t *)malloc(sizeof(struct word_t));
-							strcpy(curr.next.word, str);
-							curr.next.count++;
-
-						}
-						curr = curr.next;
-
-					}
-
-
-				}
-
-				free(str);
-				str = (char *)malloc(sizeof(char));
-					}
-			}
-			
-		}
-	}
-
-
-	
 	// for (i = 0; i < argc; i++)
 	// 	printf("\n%s", argv[i]);
-	
-	return 0;
-
-}
