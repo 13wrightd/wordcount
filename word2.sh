ddae4922cf7e0657ad32315669d50c36a10fa230
dan <13wrightd@gmail.com>
2016-02-16 03:57:55 -0500

    filenames with spaces now work

--- word2.sh
+++ word2.sh
@@ -2,13 +2,13 @@
 echo "reading file: $1"
 #read filename
 # cat grep awk sort sed cut
-cat $1 | sed 's/[^a-zA-Z0-9 _-]//g' |
+cat "$1" | sed 's/[^a-zA-Z0-9 _-]//g' |
 tr '[:upper:]' '[:lower:]' |
 sed 's/ /\n/g' |
 sed '/^\s*$/d' |
-sort |  uniq -c | awk '{print $2", "$1}' > out
+sort |  uniq -c | awk '{print $2", "$1}' > "$2"
 echo "---------------"
-cat out
+cat "$2"
 #cp $1 output
 #cat $1 | tr -cs "[:alpha:]" "\n"
 
