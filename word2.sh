53c869ebd803133b46d391988ba6142c5720d208
elg5195 <evan.gutman@gmail.com>
2016-02-18 01:43:05 -0500

    README test data added

--- word2.sh
+++ word2.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 start=$(date +%s%N)
-echo "reading file: $1"
 
-#read filename
-# cat grep awk sort sed cut
+
 cat "$1" | LC_CTYPE=C sed 's/[^0-9A-Za-z ]//g' |
 tr '[:upper:]' '[:lower:]' |
 sed 's/ /\n/g' |
 sed '/^\s*$/d' |
 sort |  uniq -c | awk '{print $2", "$1}' > "$2"
-echo "---------------"
-cat "$2"
 
 
-#end='date +%T.%N'
-#`date +%s%N`
 echo "runtime: $(echo "scale=6;($(date +%s%N) - ${start})/(1*10^09)" | bc) seconds" > "$3"
 
 
-#echo "$((end-start))" > runtimeO
-#cp $1 output
-#cat $1 | tr -cs "[:alpha:]" "\n"
-
-#sed -e 's/[[:alpha].-]//g; /^$/d' output 
-
-#cat output
 
--- wordcount.c
+++ wordcount.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-
+//Node structure for linked list
 struct word_t {
 	char *word;
 	int count;
@@ -12,57 +12,55 @@ struct word_t {
 };
 
 int main(int argc, char *argv[]) {
-	if(argc != 4){
+	if(argc != 4){ //The user has to run the program with 4 arguments
 		printf("Invalid number of arguments. Please try running again with proper number of arguments.\narg1(input_file) arg2(output_file) arg3(output_file)\n");
 	}
 	else{
 
-		int lineNum = 1;
-		clock_t begin, end;
-		double totTime;
+		clock_t begin, end;				//For recording execution time
+		double totTime;					//to store the execution tme
 
-		begin = clock();
+		begin = clock();				//begin timing
 
-	    int i = 0;
-	    FILE *fp;
-	    fp = fopen(argv[1], "r");
+	    FILE *fp;						//for the input file
+	    fp = fopen(argv[1], "r");		//open the input file
 	    char *str;
 	    char c;
-	    int numChar = 0;
-	    struct word_t *head;
-	    struct word_t *curr;
-	    bool found = false;
+	    int numChar = 0;				//Number of characters in a string
+	    struct word_t *head;			//Linked list
+	    struct word_t *curr;			//iterator
+	    bool found = false;				//for searching the linked list
 
-	    head = (struct word_t *)malloc(sizeof(struct word_t));
-	    curr = head;
+	    head = (struct word_t *)malloc(sizeof(struct word_t));	//allocate space for the linked list
+	    curr = head;	//start at the begining of the list
 
 	    str = (char *)malloc(sizeof(char));
-	    bool badCharacter=0;
+	    bool badCharacter=0;	//for handling double \n
 
-	    if (fp) {
-	        while ((c=fgetc(fp))!=EOF){
-	            if((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57)){
-	                str[numChar] = tolower(c);
-	                numChar++;
-	                str = (char *)realloc(str, (numChar+1)*sizeof(char));
-	                badCharacter=false;
+	    if (fp) {	//if the file opened
+	        while ((c=fgetc(fp))!=EOF){	//go to the end of the file
+	            if((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57)){	//if c is alphanumeric
+	                str[numChar] = tolower(c);	//lowercase it
+	                numChar++;	//add to length of the char
+	                str = (char *)realloc(str, (numChar+1)*sizeof(char));	//allocate space for the next character
+	                badCharacter=false;										//not a bad character
 	            }
 
-	            else if((c == 32 || c == 10) && !badCharacter){
+	            else if((c == 32 || c == 10) && !badCharacter){	//if c is a space of \n
 
-	                badCharacter=true;
-	                str[numChar]='\0';
-	                numChar = 0;
+	                badCharacter=true;	//handle for possible double
+	                str[numChar]='\0';	//add null character to the end of the string
+	                numChar = 0;		//reset the string length
 	                
-	                while(!found){
+	                while(!found){	//go until you find the word in the list
 
-	                	if(curr->word == NULL){
+	                	if(curr->word == NULL){	//if the current word is null add str to the node and incerment its counter 
 	                		curr->word = str;
 	                		curr->count++;
 	                		found = true;
 	                	}
 	                	
-	            		else if(strcmp(str, curr->word) < 0){
+	            		else if(strcmp(str, curr->word) < 0){	//if str comes before the current word then insert it into the list before
 	                        struct word_t *newNode;
 	                        newNode = (struct word_t *)malloc(sizeof(struct word_t));
 	                        newNode->word = curr->word;
@@ -78,7 +76,7 @@ int main(int argc, char *argv[]) {
 	                    }
 
 	                    
-	                    else if(strcmp(str, curr->word) > 0){
+	                    else if(strcmp(str, curr->word) > 0){	//if str comes after the current word either increment the iterator or add the word to the end of the list
 	                        if(curr->next == NULL){
 	                                curr->next = (struct word_t *)malloc(sizeof(struct word_t));
 	                                curr->next->word = str;
@@ -90,39 +88,40 @@ int main(int argc, char *argv[]) {
 	                        }
 	                    }
 	                	
-	                    else{
+	                    else{	//else str == curr->word so increment the count
 	                        curr->count++;
 	                        found = true;
 	                    }
 	               	}
-	               	found = false;
-	               	curr = head;
-	            	str = (char *)malloc(sizeof(char));
+	               	found = false;	//reset found
+	               	curr = head;	//reset the iterator
+	            	str = (char *)malloc(sizeof(char));	//reset str
 	            	
 	        	}
 	        }
 
 	    }
-	    fclose(fp);
+	    fclose(fp);	//close the file
 	    
-	    FILE *fp2;
-	    fp2 = fopen(argv[2], "w");
+	    FILE *fp2;	//file for data output
+	    fp2 = fopen(argv[2], "w");	//open file in writing mode
 
 
-	   	while(curr->next != NULL){
-        	fprintf(fp2, "%s, %d\n", curr->word, curr->count);
-	     	 curr = curr->next;
+	   	while(curr != NULL){	//go through the entire list
+	   		if(curr->word[0]!='\0')
+        		fprintf(fp2, "%s, %d\n", curr->word, curr->count);	//print the data to the file
+	     	curr = curr->next;	//increment the iterator
 	    }
-	    fclose(fp2);
+	    fclose(fp2);	//close the file
 
-	    end = clock();
-	    totTime = (double)(end - begin)/CLOCKS_PER_SEC;
+	    end = clock();	//stop the execution timer 
+	    totTime = (double)(end - begin)/CLOCKS_PER_SEC;	//calculate the run time
 
-	    FILE *fp3;
-	    fp3 = fopen(argv[3], "a");
+	    FILE *fp3;	//file for execution time output
+	    fp3 = fopen(argv[3], "a");	//open in append
 	    
-	    fprintf(fp3, "Total Time: %f\n", totTime);
-	    fclose(fp3);
+	    fprintf(fp3, "Runtime: %f\n", totTime);	//print time to the file
+	    fclose(fp3);	//close the file
 	}
 	return 0;
 }
